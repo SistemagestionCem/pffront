9c142247da931050796028f9351b24fcb56c8746
maxitrigo <maxitrigo@gmail.com>
2025-02-08 12:26:33 -0300

    footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,14 +1,77 @@
 export default function Footer() {
     return(
-        <div className="flex justify-between items-center p-6">
-            <div className="text-2xl font-bold text-red-500">
-                <p>MobileFix</p>
+        <div className="flex w-full flex-col m-4">
+            <div className="text-white space-y-4 w-full md:grid md:grid-cols-3 pb-4">
+            <div className="flex flex-col items-start w-full">
+            <div className="">
+                <div className="text-2xl text-red-500 font-bold">
+                    MobileCer
+                </div>
             </div>
-            <div>
-                <button className="bg-red-500 px-4 py-2 rounded-xl text-white font-bold">
-                    Login
-                </button>
+
+            <div className="flex items-center justify-center">
+                <div className="text-white">
+                Reparación rápida, resultados duraderos.
+                </div>
             </div>
+            </div>
+
+            <div className="flex flex-col items-start space-y-2">
+            <div className="text-white font-bold text-xl">
+                Legal
+            </div>
+
+            <div className="flex items-center gap-4">
+                <img
+                className="relative w-[10.09px] h-[15.63px]"
+                alt="Vector"
+                src="https://c.animaapp.com/ktouw9tT/img/vector-1.svg"
+                />
+
+                <div className="">
+                Política de privacidad
+                </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+                <img
+                className="relative w-[10.09px] h-[15.63px]"
+                alt="Vector"
+                src="https://c.animaapp.com/ktouw9tT/img/vector-1.svg"
+                />
+
+                <div className="">
+                Términos de servicio
+                </div>
+            </div>
+            </div>
+
+            <div className="space-y-2">
+            <div className="text-white font-bold text-xl text-center">
+                Contacto
+            </div>
+
+            <div className="flex gap-4 items-center justify-center">
+                <img
+                className="relative w-[25px] h-[18.75px]"
+                alt="Vector"
+                src="https://c.animaapp.com/ktouw9tT/img/vector-2.svg"
+                />
+
+                <div className="">
+                info@mobilecer.com
+                </div>
+            </div>
+            </div>
+        </div>
+
+      <div className="text-white text-center">
+        <p className="">
+          © 2025 MobileCer
+          <br />
+          Todos los derechos reservados.
+        </p>
+      </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
         })}
       </div>
       </div>
-      <div>
+      <div className="w-full">
         <Footer/>
       </div>
 
