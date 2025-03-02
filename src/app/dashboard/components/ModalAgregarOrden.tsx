cf0bb42317287036ff0c93d50165fd372e9d1d4d
SistemagestionCem <sistemagestion.cem@gmail.com>
2025-03-01 21:06:32 -0300

    Actualizo estilos de botones de orden

--- src/app/dashboard/components/ModalAgregarOrden.tsx
+++ src/app/dashboard/components/ModalAgregarOrden.tsx
@@ -434,19 +434,19 @@ const ModalAgregarOrden = ({
 
             <div className="flex flex-col gap-2 mt-4">
               <button
-                className="w-full px-4 py-2 text-black bg-gray-300 rounded-lg flex items-center justify-center gap-2 transition-colors duration-200 hover:bg-gray-400"
+                className="w-full px-4 py-2 text-white font-semibold bg-primary-500 rounded-lg flex items-center justify-center gap-2 transition-colors duration-200 hover:bg-primary-600"
                 onClick={() => {
                   resetForm();
                   onClose();
                 }}
               >
-                Cancelar
+                Cancelar Orden
               </button>
 
               <button
                 onClick={handleSubmit}
                 disabled={!isFormValid()}
-                className={`w-full px-4 py-2 text-white rounded-lg flex items-center justify-center gap-2 transition-colors duration-200 bg-primary-500 hover:bg-primary-600 ${
+                className={`w-full px-4 py-2 text-black font-semibold rounded-lg flex items-center justify-center gap-2 transition-colors duration-200 bg-green-500 hover:bg-green-600 ${
                   !isFormValid() ? "opacity-50 cursor-not-allowed" : ""
                 }`}
               >
